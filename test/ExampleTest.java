7ee08acfbedde17460dd48f6503b99b40425e503
Olcan Teke <oteke@mixcom.nl>
2022-04-29 21:05:56 +0200

    Added test folder, exampleTest and testresult.

---
+++ test/ExampleTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+
+    @Test
+    void exampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
