4ed79b69400a84c052d6d0e247430f4691eef16d
Ditmar Visser <ditmarvisser@gmail.com>
2022-05-02 13:58:01 +0200

    Create graph to internal arrays

--- src/model/graphModel.java
+++ src/model/graphModel.java
@@ -1,36 +1,70 @@
 package model;
 
 import java.sql.*;
+import model.edgeModel;
 
 public class graphModel {
 
-	public graphModel() throws SQLException {
+    private int graphID;
+    private int[] nodeList;
+    private edgeModel[] edgeList;
+
+
+	public graphModel(int numberOfNodes) {
+        this.nodeList = new int[numberOfNodes];
+
+        // calculate number of edges (summation/sigma)
+        int numberOfEdges = 0;
+        for (int i = 1; i <= numberOfNodes - 1; i++) {
+            numberOfEdges = numberOfEdges + i;
+        } 
+        this.edgeList = new edgeModel[numberOfEdges];
+
+        try {
+            createGraph();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        
+
+        System.out.println("Created a graph with " + numberOfNodes + " nodes, and " + numberOfEdges + " edges.");
+    }
+
+	public void createGraph() throws SQLException {
 
 		Connection myConnection = null;
 		Statement statement = null;
 		ResultSet result = null;
 		
 		try {
-            long startTime = System.nanoTime();
-
-            
-			// 1. connect to database and create a statement
-			myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/nerdygadgets", "root" , "");
-			statement = myConnection.createStatement();
+            // 1. connect to database and create a statement
+			myConnection = DriverManager.getConnection("jdbc:mysql://145.44.235.61:80/nerdygadgets", "student", "Welkom01!");
+			// myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/nerdygadgets", "root", "");
+			statement = myConnection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			
 			
             // 2. execute query (x random addresses) 
             // TODO change to bestellingen instead of adresses
-            int numberOfResults = 100;
-			result = statement.executeQuery("select * from adres ORDER BY RAND() LIMIT " + numberOfResults);
+			result = statement.executeQuery("select * from adres ORDER BY RAND() LIMIT " + this.nodeList.length);
 			
 			
             // 3. Calculate cost of every edge
             int startNodeID, startNodeIDX, startNodeIDY, targetNodeID, targetNodeIDX, targetNodeIDY;
+            int edgeID = 0;
+            int nodeID = 0;
+            int createdEdgesCount = 0;
             double cost;
+            boolean edgeExists;
             
+            // add every node to the nodeList
+            while(result.next()) {
+                // System.out.println(nodeID);
+                nodeList[nodeID] = result.getInt("adresID");
+                nodeID++;
+            }
+
             // for every node
-            for (int startNodeIndex = 1; startNodeIndex < numberOfResults + 1; startNodeIndex++) {
+            for (int startNodeIndex = 1; startNodeIndex < this.nodeList.length + 1; startNodeIndex++) {
                 // get the x and y coordinates
                 result.absolute(startNodeIndex); // absolute() sets the active row in the table, has to be set manually because we switch back and forth between entries
                 startNodeID = result.getInt("adresID");
@@ -38,31 +72,47 @@ public class graphModel {
                 startNodeIDY = result.getInt("y");
                 
                 // for every other node also take the x and y coordinates
-                for (int targetNodeIndex = 1; targetNodeIndex < numberOfResults + 1; targetNodeIndex++) {
-                    if (startNodeIndex != targetNodeIndex) { // TODO check if the edge is already in the database
+                for (int targetNodeIndex = 1; targetNodeIndex < this.nodeList.length + 1; targetNodeIndex++) {
+                    
+                    if (startNodeIndex != targetNodeIndex) {
                         result.absolute(targetNodeIndex); // absolute() sets the active row in the table, has to be set manually because we switch back and forth between entries
                         targetNodeID = result.getInt("adresID");
-                        targetNodeIDX = result.getInt("x");
-                        targetNodeIDY = result.getInt("y");
-                        
-                        // calculate the distance between the nodes with the hypotenuse. Both x and y distance must be positive (absolute)
-                        cost = Math.hypot(Math.abs(startNodeIDX-targetNodeIDX), Math.abs(startNodeIDY-targetNodeIDY));
-                        
-                        // TODO insert into database
-                        // System.out.println(startNodeID + " - " + targetNodeID);
-                        // System.out.println(cost);
-                        // System.out.println("");
+                        // check if the edge is already in the database
+
+                        edgeExists = false;
+                        for (int edge = 0; edge < createdEdgesCount; edge++) {
+                            if ((edgeList[edge].getStartNodeID() == startNodeID && edgeList[edge].getTargetNodeID() == targetNodeID) || 
+                            (edgeList[edge].getTargetNodeID() == startNodeID && edgeList[edge].getStartNodeID() == targetNodeID)) {
+                                edgeExists = true;
+                                break;
+                            }
+                        }
+                        if (!edgeExists) {
+                            targetNodeIDX = result.getInt("x");
+                            targetNodeIDY = result.getInt("y");
+                            
+                            // calculate the distance between the nodes with the hypotenuse. Both x and y distance must be positive (absolute)
+                            cost = Math.hypot(Math.abs(startNodeIDX-targetNodeIDX), Math.abs(startNodeIDY-targetNodeIDY));
+                            
+                            edgeModel edge = new edgeModel(edgeID, startNodeID, targetNodeID, cost);
+                            edgeList[edgeID] = edge;
+                            // TODO insert into database
+                            // System.out.println(startNodeID + " - " + targetNodeID);
+                            // System.out.println(cost);
+                            // System.out.println("");
+                            edgeID++;
+                            createdEdgesCount++;
+                        }
                     }
                 }
             }
-            long endTime = System.nanoTime();
-            long duration = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
-            System.out.println("Calculating a graph of size " + numberOfResults + " took " + duration + " milliseconds.");
 		}
-		// afhandelen van een exception wanneer het de connectie/query het niet doet.
+        
+        // afhandelen van een exception wanneer het de connectie/query het niet doet.
 		catch (Exception exception) {
             exception.printStackTrace();
-		}
+        }
+
 		finally {
 			if (result != null) {
 				result.close();
@@ -77,5 +127,4 @@ public class graphModel {
 			}
 		}
 	}
-
 }
