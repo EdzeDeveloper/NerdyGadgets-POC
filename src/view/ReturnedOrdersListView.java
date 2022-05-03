67b088a8050ce5d18b14e1d130daacfcc8f8d192
Edze van der Werff <edze@ntri.tech>
2022-05-03 23:42:34 +0200

    retrun order list samen met list componetn

--- src/view/ReturnedOrdersListView.java
+++ src/view/ReturnedOrdersListView.java
@@ -1,38 +1,39 @@
 package view;
 
-import java.awt.*;
 import javax.swing.*;
 
-import view.panels.ListPanel;
+import view.components.List;
+
+import java.util.Vector;
 
 public class ReturnedOrdersListView extends JFrame{
-	private ListPanel listPanel;
-
 	public ReturnedOrdersListView(){
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-					try {
-						UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-					} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-					} 
-					JFrame frame = new JFrame("Producten lijst");
-					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.add(new JScrollPane(listPanel));
-					frame.pack();
-					for (int i = 0; i < 12; i++) {
-						listPanel.addNewPanel("42042");
-					}
-					frame.setVisible(true);
-			}
-		});
-	}
+			try {
 
-  public void getReturnProductList(String string){
-		for (int i = 0; i < 12; i++) {
-			listPanel.addNewPanel(string);
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+			} 
+
+			JFrame frame = new JFrame( "Create list for use" );
+			
+
+			// bespreken olcan!!: generiek maken voor verschillende data
+			Vector v = new Vector();
+			for ( int i = 0; i < 10; i++ ) {
+					v.add( "Data " + i );
+			}
+
+			// add list data functie
+			List myList = new List(v, "titel");
+
+			JPanel listPanel = new JPanel();
+			listPanel.add( myList );
+			
+			frame.getContentPane().add( listPanel );
+			frame.setSize(600, 600);
+			frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+			frame.pack();
+			frame.setVisible(true);
 		}
-	}
-	
 }
 
