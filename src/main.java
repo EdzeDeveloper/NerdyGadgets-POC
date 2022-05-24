49cffb9954f3f53474f837e44b8bd6b3ca6b3828
Edze van der Werff <edze@ntri.tech>
2022-05-24 22:58:36 +0200

    t

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import controller.MainController;
+import model.newDatabaseConnection;
 public class main {
   public static void main(String[] args) throws SQLException {
 
@@ -10,10 +11,10 @@ public class main {
       
     // new ReturnOrderListController(returnOrderListView);
 
-    // ResultSet getAllPersonData = newDatabaseConnection.select("Select * from Persoon limit 10");
-    // while(getAllPersonData.next()){
-    //   System.out.println(getAllPersonData.getString("voornaam"));
-    // }
+    ResultSet getAllPersonData = newDatabaseConnection.select("Select * from Persoon limit 10");
+    while(getAllPersonData.next()){
+      System.out.println(getAllPersonData.getString("voornaam"));
+    }
     // newDatabaseConnection.insertUpdateDelete("INSERT INTO persoon (persoonID, voornaam, achternaam, adresID, email) VALUES ('69420', 'edze', 'van der werff', '3889', 's1177499@student.windesheim.nl')");
     // newDatabaseConnection.insertUpdateDelete("DELETE FROM persoon WHERE persoonID = '69420'");
     // newDatabaseConnection.insertUpdateDelete("UPDATE persoon SET voornaam = 'hotty42069' WHERE persoonID = '69420'");
